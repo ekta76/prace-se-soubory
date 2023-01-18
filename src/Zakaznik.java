30d5274410dcdb8fc2cf7a45c9a60e2cfedab24b
ekta76 <mirekmerta2005@gmail.com>
2023-01-18 16:53:49 +0100

    Přidán .gitinore a Main.java

---
+++ src/Zakaznik.java
@@ -0,0 +1,30 @@
+public class Zakaznik {
+    private String jmeno;
+    private int pocetProdeju;
+
+    public Zakaznik(String jmeno, int pocetProdeju) {
+        this.jmeno = jmeno;
+        this.pocetProdeju = pocetProdeju;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public int getPocetProdeju() {
+        return pocetProdeju;
+    }
+
+    public void setPocetProdeju(int pocetProdeju) {
+        this.pocetProdeju = pocetProdeju;
+    }
+
+    @Override
+    public String toString() {
+        return jmeno + pocetProdeju + "prodáno";
+    }
+}
